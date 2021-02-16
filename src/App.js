2c344905af858c396a71a42c7d24f6fb4dff91b4
LadeMor <55658955+LadeMor@users.noreply.github.com>
2021-02-16 16:33:58 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import TodoListTask from './Components/TodoListTask';
 import AddTasks from './Components/AddTasks';
+import CreateProject from './Components/CreateProject';
+import Project from './Components/Project';
+import ProjectTask from './Components/ProjectTask';
 
 
 class App extends React.Component{
@@ -33,7 +36,19 @@ class App extends React.Component{
           Priority:false,
           time:"12:30"
         },
-      ] 
+      ],
+      projectList:[
+        {
+          id:0,
+          projetcTitle:"First project",
+          projectTask:"Do this thing in the project"
+        },
+        {
+          id:1,
+          projetcTitle:"First project",
+          projectTask:"Do sss thing in the project"
+        }
+      ]
     }
     const { listTasks } = this.state;
   }
@@ -62,6 +77,38 @@ class App extends React.Component{
     });
   };
   
+  editTasks = (id, input, dscInput, timeInput) => {
+    const index = this.state.listTasks.map(Task => Task.id).indexOf(id);
+    this.setState(state => {
+      let { listTasks } = state;
+      listTasks[index].title = "Edit value";
+      listTasks[index].description = "Edit value";
+      listTasks[index].time = "Edit value";
+      return listTasks;
+    })
+  };
+
+  createProject = (inputProject, inputProjetcTitle) => {
+    this.setState(state => {
+      let { projectList } = state;
+      projectList.push({
+        id:this.state.projectList.length,
+        projetcTitle:inputProjetcTitle,
+        projectTask:inputProject
+      });
+      return projectList;
+    })
+  }
+
+  addTaskProject = (id, inputProject) => {
+    const index = this.state.listTasks.map(Task => Task.id).indexOf(id);
+    this.setState(state => {
+      let { projectList } = state;
+      projectList[index].projectTask = "inputProject";
+      return projectList;
+    })
+  }
+
   impTask = id => {
     const index = this.state.listTasks.map(Task => Task.id).indexOf(id);
     this.setState(state => {
@@ -85,12 +132,34 @@ class App extends React.Component{
             <TodoListTask 
               deleteTask={() => this.deleteTask(task.id)}
               impTask={() => this.impTask(task.id)}
+              editTasks={() => this.editTasks(task.id)}
               task = {task} 
               key={task.id}>
             </TodoListTask>
           ))
         }
         <AddTasks addTasks={this.addTasks}></AddTasks>
+        <div className ="contain_create">
+          <CreateProject createProject={this.createProject}></CreateProject>
+            {
+              this.state.projectList.map(project =>(
+              <Project
+              project = {project}
+              key = {project.id}
+              >  
+              </Project>
+              ))
+            }
+           {
+              this.state.projectList.map(project =>(
+              <ProjectTask
+              project = {project}
+              key = {project.id}
+              >  
+              </ProjectTask>
+              ))
+            }
+        </div>
       </div>
       )
   }
